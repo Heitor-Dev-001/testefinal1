3a3dae22bc99fae7b3bff8bd76a17dbf9c0d287f
Heitor Silva <heitorsilvanascimento@hotmail.com>
2024-11-12 14:19:59 -0300

    Add files via upload

---
+++ testefinal.py
@@ -0,0 +1,6 @@
+#Considere a string x = 'curso de python no cursoemvideo'.
+# Qual dos comandos abaixo retornaria a palavra 'curso'?
+
+x = ('curso de python no cursoemvideo')
+
+print('A palavra curso corresponde ao comando:{}'.format(x[:5]))
